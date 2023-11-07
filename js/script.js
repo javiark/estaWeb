532caad69f9f2aa7f71794e93a07473339869f5e
Javier <javier778@hotmail.com>
2023-11-07 17:53:46 -0300

    aparecer y desaparecer titulos

--- js/script.js
+++ js/script.js
@@ -50,3 +50,53 @@ function resetCard(id) {
     div.classList.toggle("hide")
     div.classList.toggle("show")
 }
+
+
+var globalID = 0
+
+function cardChange(id){
+    globalID = id
+    console.log(id)
+    const contenedor = document.getElementById(id)
+    const img = document.getElementById("imageNew")
+    const div = document.getElementById("imageHide")
+
+    img.classList.remove("show")
+    img.classList.add("hide")
+    div.classList.remove("hide")
+    div.classList.add("show")
+}
+
+
+//-----aparecer titulo-----//
+
+var titleId = 0
+
+function titleShow(id1){
+    titleId=id1
+    console.log(id1)
+    console.log("mostrar")
+    const textInside = document.getElementById(id1)
+
+    const p = document.getElementById("textInside")
+    console.log(p)
+
+    p.classList.remove("hide")
+    p.classList.add("show")
+    
+
+}
+
+function resetTitle(id1){
+    titleId=id1
+    console.log(id1)
+    console.log("borrar")
+    const textInside = document.getElementById(id1)
+
+    const p = document.getElementById("textInside")
+    console.log(p)
+
+    p.classList.toggle("hide")
+    p.classList.toggle("show")
+
+}
